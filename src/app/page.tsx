5d1dac570284e6a99c480256143ddc460f0423f4
Kelsonobd <kelsonobd@gmail.com>
2024-06-25 12:45:59 -0300

    exercicio-1-hora-condicional

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,26 @@
-import { peopleList } from "../data/peopleList";
-
-
 const Page = () => {
+  const fullTime = new Intl.DateTimeFormat('pt-BR', {
+    timeStyle: 'short',
+    hour12:false
+  }).format()
 
-  const advogado = peopleList.filter(person => person.profession === 'advogado')
+  const hour =  new Date().getHours();
+  let greeting = '';
+
+
+  if(hour >=0 && hour < 12) {
+    greeting = 'Bom dia!';
+  } else if( hour >=12 && hour < 18){
+    greeting = 'Boa tarde';
+  }else if(hour >= 18 && hour <=23) {
+    greeting = 'Boa noite'
+  }
 
   return (
-    <div>
-      <h1 className="font-bold text 2-1">Hello world</h1>
-      <h3>Algum texto qualquer</h3>
-
-      {advogado.length > 0 &&
-        <>
-          <h3>Lista de Advogados:</h3>
-          <ul>
-            {advogado.map(person =>
-              <li key={person.id}>{person.name}</li>
-              )}
-          </ul>
-        </>
-
-      }
-
+    <div className="w-screen h-screen flex flex-col justify-center items-center text-white
+    bg-gradient-to-r from-sky-500 to-indigo-500">
+      <div className="text-9xl">{fullTime}</div>
+      <div className="text-5xl font-bold">{greeting}</div>
     </div>
   );
 }
