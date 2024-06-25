56fcc7f9a0d4c6301241513824fd8c4070d664b6
Kelsonobd <kelsonobd@gmail.com>
2024-06-25 11:10:47 -0300

    filtrando listas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,19 +3,23 @@ import { peopleList } from "../data/peopleList";
 
 const Page = () => {
 
-
+  const advogado = peopleList.filter(person => person.profession === 'advogado')
 
   return (
     <div>
       <h1 className="font-bold text 2-1">Hello world</h1>
       <h3>Algum texto qualquer</h3>
 
-      {peopleList.length > 0 &&
-        <ul>
-          {peopleList.map(
-            person => <li key={person.id}>{person.name} - {person.profession}</li>
-          )}
-        </ul>
+      {advogado.length > 0 &&
+        <>
+          <h3>Lista de Advogados:</h3>
+          <ul>
+            {advogado.map(person =>
+              <li key={person.id}>{person.name}</li>
+              )}
+          </ul>
+        </>
+
       }
 
     </div>
--- src/data/peopleList.ts
+++ src/data/peopleList.ts
@@ -5,6 +5,6 @@ export const peopleList: Person[] = [
     { id: 2, name: 'Aurora', profession: 'Dog'},
     { id: 3, name: 'Patrick', profession: 'Gerente'},
     { id: 4, name: 'Mello', profession: 'Caramelo'},
-    { id: 5, name: 'Kelly', profession: 'advogada'},
+    { id: 5, name: 'Kelly', profession: 'advogado'},
     { id: 6, name: 'Ayla', profession: 'estudante'}
   ];
