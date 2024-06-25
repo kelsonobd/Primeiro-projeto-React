a921f8a6dbb84b272c8add9853d6489a9d5508d4
Kelsonobd <kelsonobd@gmail.com>
2024-06-25 09:37:58 -0300

    add-children

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,21 @@
-
-import { GeoForm } from '@/components/GeoForm';
-import { Person } from '@/components/Person';
-
+import { Card } from "@/components/Card";
 
 
 const Page = () => {
   return (
     <div>
-    <h1>Olá Mundo</h1>
-    <h3>Algum texto qualquer</h3>
+      <h1 className="font-bold text 2-1">Hello world</h1>
+      <h3>Algum texto qualquer</h3>
 
-    <Person />
-    </div>
-  );
+
+      <Card>
+        <>
+          <h3 className="text-3xl font-bold italic">Fonte de Efeito</h3>
+          <p className="text-right text-sm">- Autor desconhecido</p>
+          </>
+        </Card>
+      </div>
+      );
 }
 
-export default Page;
+      export default Page;
---
+++ src/components/Card.tsx
@@ -0,0 +1,14 @@
+import { ReactNode } from "react";
+
+type Props = {
+    children:ReactNode;
+}
+
+export const Card = ({children }: Props) => {
+    return (
+        <div className="w-96 border-2 border-red-600 p-3 text-3xl text-center">
+            {children}
+
+        </div>
+    );
+}
