02322bd94135c941aa5f5c848313d9eb7214bcdd
Kelsonobd <kelsonobd@gmail.com>
2024-06-27 10:16:53 -0300

    exercicio-emoji-numer-1

---
+++ src/components/EmojiRating.tsx
@@ -0,0 +1,20 @@
+type Props = {
+    rate: number;
+}
+
+export const EmojiRating = ({rate}: Props) => {
+    if(rate > 5) rate = 5;
+    if(rate < 0) rate = 0;
+
+    const rateInt = Math.floor(rate);
+
+    const stars = '😄'.repeat(rateInt) + '😶'.repeat(5 - rateInt);
+
+
+    return (
+        <div className="flex items-center text-6xl">
+        <div className="bg-gray-300 p-2 rounred">{rate.toFixed(1)}</div>
+        <div className="ml-3">{stars}</div>
+        </div>
+    );
+}
