9d2b110b167d289db4e9dbfa1ca2846be2228d3c
Kelsonobd <kelsonobd@gmail.com>
2024-07-10 09:26:15 -0300

    eventos via props

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,18 @@ import { CustomButton } from "@/components/CustomButtom";
 
 const Page = () =>{
 
+  const handleButton1 = () => alert('clicou no botao 1');
+    const handleButton2 = () =>  alert('clicou no botao 2');
+      const handleButton3 = () =>  alert('clicou no botao 3');
+  
+  
+
 
   return(
     <div className="w-screenh h-screen flex justify-center items-center">
-      <CustomButton label="Clique Aqui" />
-      <CustomButton label="Clique Aqui 2" />
-      <CustomButton label="Clique Aqui 3" />
+      <CustomButton label="Clique Aqui" onClick={handleButton1} />
+      <CustomButton label="Clique Aqui 2" onClick={handleButton2}/>
+      <CustomButton label="Clique Aqui 3" onClick={handleButton3}/>
     </div>
   );
 }
